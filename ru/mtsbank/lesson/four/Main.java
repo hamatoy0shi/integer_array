73bc8e8c70789b7cb296ad384020699a3d599958
Ivan <ivan@Ivans-MacBook-Air.local>
2023-03-20 13:30:22 +0300

    Сумма чётных элементов целочисленного массива

--- ru/mtsbank/lesson/four/Main.java
+++ ru/mtsbank/lesson/four/Main.java
@@ -1,17 +1,13 @@
 package ru.mtsbank.lesson.four;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = {-1,0,25,7,-4,10,5,8};
-        int sum = 0;
+        int[] array = {-1, 0, 25, 7, -4, 10, 5, 8};
 
-        for (int value : array){
-            if (value%2 == 0)
-                sum += value;
-        }
-
-        System.out.println("Сумма чётных элементов массива: " + sum);
-
+        System.out.println("Сумма чётных элементов массива: " + Arrays.stream(array).filter(i -> i % 2 == 0).sum());
     }
 
 }
